894c2fa3077fd0c4a608a32094c955631026f96e
Denis Laure <den.a.laure@gmail.com>
2015-11-28 19:23:16 +0100

    Add possibility to get all dates in the diary from database

--- app/src/main/java/se/kth/eit/trackit/persistence/DiaryEntriesDAO.java
+++ app/src/main/java/se/kth/eit/trackit/persistence/DiaryEntriesDAO.java
@@ -1,10 +1,15 @@
 package se.kth.eit.trackit.persistence;
 
 import com.j256.ormlite.dao.BaseDaoImpl;
+import com.j256.ormlite.stmt.PreparedQuery;
+import com.j256.ormlite.stmt.QueryBuilder;
 import com.j256.ormlite.support.ConnectionSource;
 import se.kth.eit.trackit.model.DiaryEntry;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 /**
  * Data Access Object for diary entries.
@@ -20,5 +25,22 @@ public class DiaryEntriesDAO extends BaseDaoImpl<DiaryEntry, Integer> {
     protected DiaryEntriesDAO(ConnectionSource connectionSource, Class<DiaryEntry> dataClass)
             throws SQLException {
         super(connectionSource, dataClass);
+    }
+
+    /**
+     * Returns list of all dates in the diary.
+     * @return list of all dates in the diary.
+     * @throws SQLException if failed to execute SQL query.
+     */
+    public List<Date> getAllDates() throws SQLException {
+        QueryBuilder<DiaryEntry, Integer> queryBuilder = this.queryBuilder();
+        PreparedQuery<DiaryEntry> preparedQuery = queryBuilder.distinct()
+                .selectColumns(DiaryEntry.DATE_FIELD_NAME).prepare();
+        List<DiaryEntry> diaryEntries = this.query(preparedQuery);
+        List<Date> dates = new ArrayList<>();
+        for (DiaryEntry entry : diaryEntries) {
+            dates.add(entry.getDate());
+        }
+        return dates;
     }
 }
